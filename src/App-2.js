9636ad2c4bdadfa3827c27dfee1adc65e6ebab42
Andy-W <andy.bartkiv@gmail.com>
2021-11-05 20:49:20 +0300

    app-2 started

---
+++ src/App-2.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Clock from './clock';
+import './App.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                APP-2
+                <Clock />
+            </div>
+        )
+    }
+}
+
+export default App;
